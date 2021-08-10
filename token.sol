de86a1df7d511b1f40816322cfec2dea2b45cffe
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2021-08-10 12:10:06 +0200

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CWJToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("CodeWithJoe", "CWJ") {
-        _mint(msg.sender, initialSupply);
+contract CodeWithJoe is ERC20 {
+    constructor() ERC20("CodeWithJoe", "CWJ") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
     }
 }
